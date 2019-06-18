b9844ea456c015dd5094ba9b204a912336f0b4a6
lushusheng <lushushen8@163.com>
2019-06-19 00:50:11 +0800

    commit files

--- src/main/java/com/example/happy/controller/HelloController.java
+++ src/main/java/com/example/happy/controller/HelloController.java
@@ -1,15 +1,18 @@
 package com.example.happy.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@Slf4j
 public class HelloController {
 
     @GetMapping("/say")
     public String sayHello() {
+        log.info("hello, this is ubuntu saying hello to you !");
         return "hello, this is ubuntu saying hello to you !";
     }
 }
