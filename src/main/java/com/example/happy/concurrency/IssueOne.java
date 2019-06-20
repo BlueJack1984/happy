d8c9f45748d25c8c3a2c47644003a19c20b04ea5
lushusheng <lushushen8@163.com>
2019-06-21 00:17:18 +0800

    commit files

--- src/main/java/com/example/happy/concurrency/IssueOne.java
+++ src/main/java/com/example/happy/concurrency/IssueOne.java
@@ -6,6 +6,13 @@ public class IssueOne {
 
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis());
+        try {
+            concurrency();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        serial();
+
     }
 
     private static void concurrency()throws InterruptedException {
@@ -24,5 +31,22 @@ public class IssueOne {
         for(long i = 0; i < count; i ++) {
             b --;
         }
+        thread.join();
+        long time = System.currentTimeMillis() - start;
+        System.out.println("concurrency:" + time + " ms, b = " + b);
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for(long i = 0; i < count; i ++) {
+            a += 5;
+        }
+        int b = 0;
+        for(long i = 0; i < count; i ++) {
+            b --;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial:" + time + " ms, b = " + b);
     }
 }
